5647e094842ad446a0847f75fe33b61e665cb573
Shobha K <shobha020@gmail.com>
2017-05-20 02:43:15 -0500

    Update sumarrystats.py

--- summarystats.py
+++ summarystats.py
@@ -1,8 +1,4 @@
 
-# coding: utf-8
-
-# In[13]:
-
 import pandas as pd
 import seaborn as sns #Python visualization library
 import os
@@ -11,23 +7,16 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('./stats/out3.csv')
 data=data[data['DRG type']=="MED"]
 
-# In[14]:
-
 data.info()
 
 for column in ['average_covered_charges', 'average_total_payments', 'average_medicare_payments']:
     data[column] = pd.to_numeric(data[column])
-
-# In[15]:
 
 agg_columns = ['mean', 'median', 'var', 'std', 'count', 'min', 'max']
 groupby_mdc = data[['mdc_id', 'average_total_payments']].groupby(by='mdc_id').agg(agg_columns)
 groupby_mdc.columns = [header + '-' + agg_column
                        for header, agg_column in zip(groupby_mdc.columns.get_level_values(0), agg_columns)]
 groupby_mdc.columns = groupby_mdc.columns.get_level_values(0)
-
-
-# In[16]:
 
 groupby_mdc.reset_index(inplace=True)
 groupby_mdc['average_total_payments-range'] = groupby_mdc['average_total_payments-max'] - groupby_mdc['average_total_payments-min']
